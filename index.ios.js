dd535886b82d213d32812bc3a3e6d74efecc18e2
Mengli Ding <mlding@thoughtworks.com>
2017-09-01 16:01:58 +0800

    animate to scale when press in and out

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Animated, Easing } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Animated, Easing, TouchableWithoutFeedback } from 'react-native';
 
 export default class animation extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePressIn = this.handlePressIn.bind(this);
+    this.handlePressOut = this.handlePressOut.bind(this);
+  }
 
   componentWillMount() {
       this.animatedValue = new Animated.Value(100);
+      this.pressAnimatedValue = new Animated.Value(1);
   }
 
   componentDidMount() {
@@ -12,15 +18,40 @@ export default class animation extends Component {
       toValue: 150,
       duration: 1000,
       easing: Easing.bounce
+    }).start()
+  }
+
+  handlePressIn() {
+    Animated.spring(this.pressAnimatedValue, {
+      toValue: 0.5
+    }).start()
+  }
+
+  handlePressOut() {
+    Animated.spring(this.pressAnimatedValue, {
+      toValue: 1,
+      friction: 3,
+      tension: 40
     }).start();
   }
 
   render() {
     const animatedStyle = {height: this.animatedValue};
+    const pressAnimatedStyle = {
+      transform: [{scale: this.pressAnimatedValue}]
+    };
 
     return (
       <View style={styles.container}>
         <Animated.View style={[styles.box, animatedStyle]} />
+        <TouchableWithoutFeedback
+          onPressIn={this.handlePressIn}
+          onPressOut={this.handlePressOut}
+        >
+          <Animated.View style={[styles.button, pressAnimatedStyle]}>
+            <Text style={styles.text}>Press me</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
@@ -37,6 +68,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
     width: 100,
     height: 100
+  },
+  button: {
+    backgroundColor: 'black',
+    width: 100,
+    height: 50
+  },
+  text: {
+    color: 'white',
+    fontSize: 18
   }
 });
 
